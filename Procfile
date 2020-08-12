0a2c90774c46865416430d47d0f904a41410a9ae
Sagor <mbrsagor@gmail.com>
2020-08-13 00:30:26 +0600

    Deploy server

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn demoBlog.wsgi
--- demoBlog/settings.py
+++ demoBlog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wr_fgzs8@6$bk+43(y^=yrl5p%0jw8k3lxrqkf2zllo$eqco&j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
