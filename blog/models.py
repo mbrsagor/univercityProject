cbf10341fd050fe3837a9a64dc39bc6402fcf7fd
Sagor <mbrsagor@gmail.com>
2020-08-29 07:56:38 +0600

    models domain entity added and bootstrap add from load

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 
-class Category(models.Model):
+class DomainEntity(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(DomainEntity):
     name = models.CharField(max_length=70)
     is_active = models.BooleanField(default=True)
 
@@ -9,13 +17,11 @@ class Category(models.Model):
         return self.name
 
 
-class Article(models.Model):
+class Article(DomainEntity):
     title = models.CharField(max_length=150)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='article_category')
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='article')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
