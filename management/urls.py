efbdd76220b37a2ccc6f2c944ee4c25c8664f580
Sagor <mbrsagor@gmail.com>
2020-09-02 08:15:15 +0600

    User profile created

---
+++ management/urls.py
@@ -0,0 +1,1 @@
+from django.urls import pathfrom .views import Profileurlpatterns = [    path('profile/', Profile.as_view(), name='profile')]
--- management/views.py
+++ management/views.py
@@ -1,3 +1,1 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import TemplateViewclass Profile(TemplateView):    template_name = "profile/my_profile.html"
